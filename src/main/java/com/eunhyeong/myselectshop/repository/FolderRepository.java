8a8e72aee9b5f71f384f8540a31f4e2d4cf0d329
Jo Eun Hyeong <whdmsgud8@naver.com>
2024-09-04 19:44:22 +0900

    폴더 추가하기

--- src/main/java/com/eunhyeong/myselectshop/repository/FolderRepository.java
+++ src/main/java/com/eunhyeong/myselectshop/repository/FolderRepository.java
@@ -1,7 +1,13 @@
 package com.eunhyeong.myselectshop.repository;
 
 import com.eunhyeong.myselectshop.entity.Folder;
+import com.eunhyeong.myselectshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FolderRepository extends JpaRepository<Folder,Long> {
+    List<Folder> findAllByUserAndNameIn(User user, List<String> folderNames);
+
+    List<Folder> findAllByUser(User user);
 }
