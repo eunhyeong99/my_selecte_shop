1a54df02d338659e30daa98f3c8e8db7d25949fc
eunhyeong99 <132648987+eunhyeong99@users.noreply.github.com>
2024-09-18 21:28:15 +0900

    Top5 회원 찾기 구현

---
+++ src/main/java/com/eunhyeong/myselectshop/repository/ApiUseTimeRepository.java
@@ -0,0 +1,11 @@
+package com.eunhyeong.myselectshop.repository;
+
+import com.eunhyeong.myselectshop.entity.ApiUseTime;
+import com.eunhyeong.myselectshop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ApiUseTimeRepository extends JpaRepository<ApiUseTime, Long> {
+    Optional<ApiUseTime> findByUser(User user);
+}
